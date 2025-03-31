36467c18195d4b63b4c7b3fd2b8bfdcbc942a299
Hnzuonee <harvillajan@gmail.com>
2025-03-31 20:52:48 +0200

    Update redirect.js

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 302,
     headers: {
-      'Location': 'https://onlyfans.com/tvuj-cilovy-profil', // <- Změň na svůj profil!
+      'Location': 'https://onlyfans.com/barbora.pribylova', // <- Změň na svůj profil!
       'Cache-Control': 'no-cache',
     },
   };
