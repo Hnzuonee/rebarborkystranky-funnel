2483f05a7f0b5c936b151909d0a39383764581eb
Hnzuonee <harvillajan@gmail.com>
2025-04-18 15:50:47 +0200

    Delete netlify/functions/redirect.js

--- netlify/functions/redirect.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 302,
-    headers: {
-      'Location': 'https://onlyfans.com/barbarella_xoxo', // <- Změň na svůj profil!
-      'Cache-Control': 'no-cache',
-    },
-  };
-};
