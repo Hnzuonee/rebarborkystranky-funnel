84c17ad66217155b700b7917786b91233a142afd
Hnzuonee <harvillajan@gmail.com>
2025-04-18 15:51:33 +0200

    Create redirect.js

---
+++ functions/redirect.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: 'https://onlyfans.com/barbarella_xoxo',
+      'Cache-Control': 'no-cache'
+    },
+    body: ''
+  };
+};
