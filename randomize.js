7634028845015a6dd626092f239a598c89afeb13
Hnzuonee <harvillajan@gmail.com>
2025-04-24 18:07:53 +0200

    Delete randomize.js

--- randomize.js
+++
@@ -1,7 +0,0 @@
-let html = fs.readFileSync('public/index.html', 'utf8')
-              .replace(/{{SALT}}/g, salt);
-fs.writeFileSync('public/index.html', html);
-
-let css  = fs.readFileSync('public/style.css', 'utf8')
-              .replace(/{{SALT}}/g, salt);
-fs.writeFileSync('public/style.css', css);
